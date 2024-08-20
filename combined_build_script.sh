370bc0d79c6dadbd01d443c2012b75457236cff0
Akhil Shekar (Radha Machine) <akhil.shekar@amd.com>
2024-08-20 19:49:12 +0000

    Housekeeping - changes to build script and git ignore files

--- combined_build_script.sh
+++ combined_build_script.sh
@@ -1,8 +1,8 @@
 rm -rfv record-mode-llama-cli replay-mode-llama-cli
 make clean
-make GGML_HIPBLAS=1 -j12 CPPFLAGS="-UREPLAY_MODE"
+make GGML_HIPBLAS=1 -j12 CPPFLAGS="-UREPLAY_MODE" llama-cli
 mv -v llama-cli record-mode-llama-cli
 make clean
-make GGML_HIPBLAS=1 -j12 CPPFLAGS="-DREPLAY_MODE"
+make GGML_HIPBLAS=1 -j12 CPPFLAGS="-DREPLAY_MODE" llama-cli
 mv -v llama-cli replay-mode-llama-cli
 make clean
