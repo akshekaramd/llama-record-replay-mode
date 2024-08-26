00990aa8887a08ba791ec6727417e8c3c17360c7
Akhil Shekar <akhilshekar@amd.com>
2024-08-26 19:50:05 +0000

    New Build scripts

---
+++ combined_build_script_aie.sh
@@ -0,0 +1,11 @@
+rm -rfv record-mode-llama-cli replay-mode-llama-cli
+make clean
+make GGML_HIPBLAS=1 -j12 CPPFLAGS="-UREPLAY_MODE -DAIE_MODE" llama-cli
+mv -v llama-cli record-mode-llama-cli
+make clean
+make GGML_HIPBLAS=1 -j12 CPPFLAGS="-DREPLAY_MODE -DAIE_MODE" llama-cli
+mv -v llama-cli replay-mode-llama-cli
+make clean
+
+rm -rf saved_values/
+mkdir saved_values/
--- combined_build_script_pim.sh
+++ combined_build_script_pim.sh
@@ -1,9 +1,9 @@
 rm -rfv record-mode-llama-cli replay-mode-llama-cli
 make clean
-make GGML_HIPBLAS=1 -j12 CPPFLAGS="-UREPLAY_MODE" llama-cli
+make GGML_HIPBLAS=1 -j12 CPPFLAGS="-UREPLAY_MODE -UAIE_MODE" llama-cli
 mv -v llama-cli record-mode-llama-cli
 make clean
-make GGML_HIPBLAS=1 -j12 CPPFLAGS="-DREPLAY_MODE" llama-cli
+make GGML_HIPBLAS=1 -j12 CPPFLAGS="-DREPLAY_MODE -UAIE_MODE" llama-cli
 mv -v llama-cli replay-mode-llama-cli
 make clean
 
